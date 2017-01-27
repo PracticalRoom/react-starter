c8e3492d22c369af574b61335e9ba2bf513e6a59
Mario Pabon <me@mariopabon.com>
2017-01-27 18:13:08 -0500

    Set up tests

---
+++ test/components/home.js
@@ -0,0 +1,11 @@
+import Home from '../../app/routes/home';
+import React from 'react';
+import expect from 'expect';
+import {shallow} from 'enzyme';
+
+describe('Rendering a <Home /> route', function () {
+  it('renders text', function () {
+    const wrapper = shallow(<Home />);
+    expect(wrapper.text()).toContain('React Starter / Underdog.io');
+  })
+});
